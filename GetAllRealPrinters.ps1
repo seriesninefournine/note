039b1ef29e01d35c199ca94d223a4f300202dbcf
seriesninefournine <78902612+seriesninefournine@users.noreply.github.com>
2022-08-29 17:17:09 +0500

    Update GetAllRealPrinters.ps1

--- GetAllRealPrinters.ps1
+++ GetAllRealPrinters.ps1
@@ -1,43 +1,53 @@
 Clear-host
+
+$BackUpDriverPath = "c:\temp\printdrivers\"
+$StartScript = Get-Date
+Write-Host '------------------------------------------------------'
+Write-Host '-------------Загрузка логов печати--------------------'
+
 $PrintEvent = Get-WinEvent `
 -LogName 'Microsoft-Windows-PrintService/Operational' `
 -FilterXPath '<QueryList><Query Id="0"><Select>*[System[EventID=307]]</Select></Query></QueryList>'
-$lang = $null
-if (!$lang) {$lang = (Get-ItemProperty 'HKCU:\Control Panel\Desktop' PreferredUILanguages -ErrorAction SilentlyContinue ).PreferredUILanguages[0]}
-if (!$lang) {$lang = (Get-Culture).Name}
+
+Write-Host ''
+Write-Host "Прошло: $([math]::Round($(($(Get-Date) - $StartScript).TotalSeconds),2)) секунд"
+Write-Host '------------------------------------------------------'
+Write-Host '--------------Парсинг логов печати--------------------'
+Write-Host ''
 
 $PrintData = foreach ($PrintEventLine in $PrintEvent) {
-        if ($lang -eq 'en-US') {
-        $Message = $PrintEventLine.Message `
-            -replace '^Document \d+\, Print Document owned by ', '' `
-            -replace ' on ', '|' `
-            -replace ' was printed', '' `
-            -replace ' through port ', '|' `
-            -replace '\.  Size in bytes.+', '' `
-            -replace ' ', ''
+       if ($PrintEventLine.Message[0] -eq 'D') {
+        $PrintEventLine.Message -match '^Document.+owned by ([a-zA-Z\-\.]+) on ([\da-zA-Z\-\._\\]+).+on ([ \da-zA-Zа-яА-Я\-\._\(\)]+) .+port ([\da-zA-Z\-\._]+)\..+$'  | Out-Null          
     }
-    if ($lang -eq 'ru-RU') {
-        $Message = $PrintEventLine[0].Message `
-            -replace '^Документ \d+\, Печать документа, которым владеет ', '' `
-            -replace ' на ', '|' `
-            -replace '\, был распечатан', '' `
-            -replace ' через порт ', '|' `
-            -replace '\.  Размер в байтах.+', '' `
-            -replace ' ', ''
+    if ($PrintEventLine.Message[0] -eq 'Д') {
+        $PrintEventLine.Message -match '^Документ.+владеет ([a-zA-Z\-\.]+) на ([\da-zA-Z\-\._\\]+).+на ([ \da-zA-Zа-яА-Я\-\._\(\)]+) .+порт ([\da-zA-Z\-\._]+)\..+$'  | Out-Null 
     }
-
-    $Message -match '^([a-zA-Z.]+)\|([a-zA-Z0-9\-\\]+)\|([a-zA-Z0-9\-_\(\)]+)\|(.+$)' | Out-Null
+       
+       if (!(Get-Printer -Name $Matches[3] -ErrorAction SilentlyContinue)) {
+             continue
+       }
+       
     New-Object PSObject -Property @{
-                User            = $Matches[1]
-        Workstation     = $Matches[2]
+             User            = $Matches[1]
+        Workstation     = $Matches[2] -replace "\\", ''
         PrinterName     = $Matches[3]
         PrinterPortName = $Matches[4]
 
     }
 }
-
 $PrintData = $PrintData | select * -Unique
+
+Write-Host ''
+Write-Host "Прошло: $([math]::Round($(($(Get-Date) - $StartScript).TotalSeconds),2)) секунд"
+Write-Host '------------------------------------------------------'
+Write-Host '-------------Загрузка базы драйверов------------------'
+
 $DriverVersionList = Get-WindowsDriver -Online -All | Select-Object -Property OriginalFileName,Version
+
+Write-Host ''
+Write-Host "Прошло: $([math]::Round($(($(Get-Date) - $StartScript).TotalSeconds),2)) секунд"
+Write-Host '------------------------------------------------------'
+Write-Host '----------Выборка дополнительных данных---------------'
 
 $PrintData | Foreach {
 
@@ -47,9 +57,26 @@ $PrintData | Foreach {
     Add-Member -InputObject $_ -NotePropertyName "PrinterComment"     -NotePropertyValue (Get-Printer -Name $_.PrinterName).Comment
     Add-Member -InputObject $_ -NotePropertyName "PrinterShare"       -NotePropertyValue (Get-Printer -Name $_.PrinterName).ShareName
     Add-Member -InputObject $_ -NotePropertyName "DriverName"         -NotePropertyValue (Get-Printer -Name $_.PrinterName).DriverName
-    Add-Member -InputObject $_ -NotePropertyName "DriverVersion"      -NotePropertyValue ($DriverVersionList | Select-Object  -Property OriginalFileName,Version | Where-Object -Property OriginalFileName -Like ((Get-PrinterDriver -name (Get-Printer -Name $_.PrinterName).DriverName | Select-Object InfPath)[0]).InfPath).Version
-
+    Add-Member -InputObject $_ -NotePropertyName "DriverVersion"      -NotePropertyValue ($DriverVersionList | Where-Object -Property OriginalFileName -Like ((Get-PrinterDriver | where name -Like  ((Get-Printer -Name $_.PrinterName).DriverName  -replace "\[", '`[' -replace ']', '`]') | Select-Object InfPath)[0]).InfPath).Version
+       $DriverInf = ($DriverVersionList | Where-Object -Property OriginalFileName -Like ((Get-PrinterDriver | where name -Like  ((Get-Printer -Name $_.PrinterName).DriverName  -replace "\[", '`[' -replace ']', '`]') | Select-Object InfPath)[0]).InfPath).OriginalFileName
+    Add-Member -InputObject $_ -NotePropertyName "DriverPath"              -NotePropertyValue ($BackUpDriverPath+((Get-ChildItem($DriverInf)).directory).Name)
+       Add-Member -InputObject $_ -NotePropertyName "DriverFullInf"        -NotePropertyValue $DriverInf
+       Add-Member -InputObject $_ -NotePropertyName "DriverInf"            -NotePropertyValue ((Get-ChildItem($DriverInf)).Name)
 }
 
-$PrintData | Export-Csv -Path "C:\temp\Printer_export_$env:COMPUTERNAME.csv" -NoTypeInformation -Encoding UTF8 -Force
-$PrintData | Out-GridView
+Write-Host ''
+Write-Host "Прошло: $([math]::Round($(($(Get-Date) - $StartScript).TotalSeconds),2)) секунд"
+Write-Host '------------------------------------------------------'
+Write-Host '----------------Выгрузка драйверов--------------------'
+
+$PrintData | select DriverFullInf -Unique | Foreach {
+       cp ((Get-ChildItem($_.DriverFullInf)).directoryname) $BackUpDriverPath -Recurse -Force
+}
+
+$PrintData | select * -ExcludeProperty DriverFullInf | Export-Csv -Path "C:\temp\Printer_export_$env:COMPUTERNAME.csv" -NoTypeInformation -Encoding UTF8 -Force
+$PrintData | select * -ExcludeProperty DriverFullInf | Out-GridView
+Write-Host ''
+Write-Host "Прошло: $([math]::Round($(($(Get-Date) - $StartScript).TotalSeconds),2)) секунд"
+Write-Host '------------------------------------------------------'
+Write-Host '-----------------Работа завершена---------------------'
+
